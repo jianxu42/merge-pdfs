a74a4c373509f73b1805adf3353decbd894e4ed6
Scott <fgsoap@gmail.com>
2023-07-26 08:44:18 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -14,17 +14,17 @@ def merge_pdfs():
     base64_content2 = data.get('file2')
     pdf_bytes1 = base64.b64decode(base64_content1)
     pdf_bytes2 = base64.b64decode(base64_content2)
-    with open("file1.pdf", "wb") as file:
+    with open("/tmp/file1.pdf", "wb") as file:
         file.write(pdf_bytes1)
-    with open("file2.pdf", "wb") as file:
+    with open("/tmp/file2.pdf", "wb") as file:
         file.write(pdf_bytes2)
 
     # Open the first PDF file
-    pdf1 = open('file1.pdf', 'rb')
+    pdf1 = open('/tmp/file1.pdf', 'rb')
     pdf1_reader = PyPDF2.PdfReader(pdf1)
 
     # Open the second PDF file
-    pdf2 = open('file2.pdf', 'rb')
+    pdf2 = open('/tmp/file2.pdf', 'rb')
     pdf2_reader = PyPDF2.PdfReader(pdf2)
 
     # Create a new PDF writer object
@@ -41,7 +41,7 @@ def merge_pdfs():
         pdf_writer.add_page(page)
 
     # Write the merged PDF file to disk
-    pdf_output = open('merged.pdf', 'wb')
+    pdf_output = open('/tmp/merged.pdf', 'wb')
     pdf_writer.write(pdf_output)
 
     # Close the input and output files
@@ -50,7 +50,7 @@ def merge_pdfs():
     pdf_output.close()
 
     # Open the PDF file
-    with open('merged.pdf', 'rb') as f:
+    with open('/tmp/merged.pdf', 'rb') as f:
         pdf_bytes = f.read()
         base64_string = base64.b64encode(pdf_bytes).decode('utf-8')
 
